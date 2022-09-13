0adca5a9542cff3b01e184e6290789abe0ad80a4
Felipe Nunes <59563220+fln99@users.noreply.github.com>
2022-09-13 13:27:33 -0300

    Create swapvalues.c

---
+++ swapvalues.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *x, int *y);
+
+int main(void) {
+  int number1 = 10;
+  int number2 = 30;
+  
+  printf("number1 is: %i and number2 is: %i\n", number1, number2);
+  
+  swap(&number1, &number2);
+  
+  printf("Now number1 is: %i and number2 is: %i\n", number1, number2);
+}
+
+void swap(int *x, int *y) {
+  int tmp;
+  tmp = *x;
+  *x = *y;
+  *y = tmp;
+}
